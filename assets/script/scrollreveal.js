1cbcd4471bd34673417916bd44d6f1664b3ef1fd
Hélio Schneider <helioschndr@gmail.com>
2023-02-07 19:17:02 -0300

    Version - 0.4

--- assets/script/scrollreveal.js
+++ assets/script/scrollreveal.js
@@ -4,12 +4,12 @@ sr.reveal(`#apresentation`,
 
     {
     duration: 2000,
-    rotate:{ x: 80, y: 80, z: 0} 
+    rotate:{ x: 80, y: 0, z: 0} 
     }
     
 )
 
-sr.reveal(`#about-me`, 
+sr.reveal(`#skills`, 
 
     {
     duration: 2000,
